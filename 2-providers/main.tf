002c00f07e2d8e33186f75cc60e42670ff0694c3
Joseph Zamora Sampson <jzamora@datasoft.co.cr>
2020-09-03 10:19:21 -0600

    first template

--- 2-providers/main.tf
+++ 2-providers/main.tf
@@ -1,9 +1,15 @@
+//before must install terraform and the <provider> cli
 terraform{
     required_version = ">=0.13.1"
 }
+//before must run aws configure --profile <name> command
+//set the key and private key
+//account id it is a identifier aws account
 provider "aws"{
     region = "us-east-2"
     allowed_account_ids = ["433695544925"]
     profile ="terraform"
-    
+
 }
+//After must run the terraform init command
+//tag name is terraform-aws
