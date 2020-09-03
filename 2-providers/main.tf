80f81c4102eb7ece9cb4d2adc700725b9cb837ba
Joseph Zamora Sampson <jzamora@datasoft.co.cr>
2020-09-03 07:50:39 -0600

    Template

--- 2-providers/main.tf
+++ 2-providers/main.tf
@@ -1,7 +1,9 @@
 terraform{
-    
+    required_version = ">=0.13.1"
 }
 provider "aws"{
-    aws
-    as
+    region = "us-east-2"
+    allowed_account_ids = ["433695544925"]
+    profile ="terraform"
+    
 }
---
+++ 3-default-template/main.tf
@@ -0,0 +1,9 @@
+terraform{
+    required_version = ">=0.13.1"
+}
+provider "aws"{
+    region = "us-east-2"
+    allowed_account_ids = ["433695544925"]
+    profile ="terraform"
+    
+}
